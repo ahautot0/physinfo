36ee612c7562f206207aa2bf4db70bf0f0b06b01
ahautot0 <ahautot@ulg.ac.be>
2018-01-03 23:53:37 +0100

    ok 3/1/2018

--- template/footer.php
+++ template/footer.php
@@ -11,10 +11,10 @@
 <div id="articlesRecents">
 	<h5>ARTICLES RECENTS</h5>
 	<ul>
-		<li><a href="<?php echo $_POST['ROOT']; ?>chroniques/vanveen.html">Jeroen van Veen, chantre du minimalisme (30/10/2017)</a></li>
+		<li><a href="<?php echo $_POST['ROOT']; ?>chroniques/rihm.html">Wolfgang Rihm (07/01/2018)</a></li>
+        <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/vanveen.html">Jeroen van Veen, chantre du minimalisme (30/10/2017)</a></li>
         <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/notation.html">La notation musicale (08/10/2017)</a></li>
-        <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/arithmetique.html">Gammes et Tempéraments (15/08/2017)</a></li>
-
+        
 	</ul>
 </div>
 
