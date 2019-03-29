37d9ceb58a77bef616ffbd2612233b635c05c31c
ahautot0 <ahautot@ulg.ac.be>
2019-03-29 23:55:34 +0100

    ok

--- template/footer.php
+++ template/footer.php
@@ -11,9 +11,10 @@
 <div id="articlesRecents">
 	<h5>ARTICLES RECENTS</h5>
 	<ul>
-    	<li><a href="<?php echo $_POST['ROOT']; ?>chroniques/blacher.html">Boris Blacher &amp; Gottfried von Einem (28/02/2019)</a></li>
+    	<li><a href="<?php echo $_POST['ROOT']; ?>chroniques/pichon.html">Raphaël Pichon dirige Pygmalion (31/03/2019)</a></li>
+        <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/blacher.html">Boris Blacher &amp; Gottfried von Einem (28/02/2019)</a></li>
         <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/nagel.html">Le Livre pour Piano de Florent Nagel (29/01/2019)</a></li>
-        <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/gorecki.html">Henryk Gorecki (06/01/2019)</a></li>
+        
           	     
 	</ul>
 </div>
