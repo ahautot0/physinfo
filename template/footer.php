7508d632d509c28ba785b553dc66c6432f5fe5b6
ahautot0 <ahautot@ulg.ac.be>
2020-02-26 00:01:48 +0100

    ok

--- template/footer.php
+++ template/footer.php
@@ -11,7 +11,7 @@
 <div id="articlesRecents">
 	<h5>ARTICLES RECENTS</h5>
 	<ul>
-        <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/sibelius.html">Jean Sibelius : l'oeuvre pour orchestre (&amp; Voix) (05/02/2020)</a></li>
+        <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/sibelius.html">Jean Sibelius : l'oeuvre pour Orchestre (&amp; Voix) (05/02/2020)</a></li>
     	<li><a href="<?php echo $_POST['ROOT']; ?>chroniques/bartok.html">Bartók, une modernité exemplaire (04/12/2019)</a></li>
     	<li><a href="<?php echo $_POST['ROOT']; ?>chroniques/dindy.html">Vincent d'Indy (17/10/2019)</a></li>
 
