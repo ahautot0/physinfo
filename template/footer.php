8c49068fde0d8f14c9be2ec59e4de539d74abb79
ahautot0 <ahautot@ulg.ac.be>
2020-04-27 00:10:58 +0200

    ok

--- template/footer.php
+++ template/footer.php
@@ -11,10 +11,10 @@
 <div id="articlesRecents">
 	<h5>ARTICLES RECENTS</h5>
 	<ul>
+        <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/symphonie1.html">La symphonie (I) : des origines à la disparition de Mahler (26/04/2020)</a></li>
         <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/jarvi.html">Kristjan Järvi à l'OPL (02/03/2020)</a></li>
         <li><a href="<?php echo $_POST['ROOT']; ?>chroniques/sibelius.html">Jean Sibelius : l'oeuvre pour Orchestre (&amp; Voix) (05/02/2020)</a></li>
-    	<li><a href="<?php echo $_POST['ROOT']; ?>chroniques/bartok.html">Bartók, une modernité exemplaire (04/12/2019)</a></li>
-
+    	
 	</ul>
 </div>
 
